5bd6640392ddb3892d3b6a6e489b4165deab391c
jonesjeff <jeffjonesproductions@gmail.com>
2022-03-29 17:25:38 +1300

    Added in color generator class

--- main/ColorGen.py
+++ main/ColorGen.py
@@ -1,6 +1,39 @@
+from ctypes import sizeof
 import bpy
+import os
+import re
+import copy
+import time
+import json
 import random
+import importlib
+from functools import partial
 from mathutils import Color
+
+#Color dict which uses a letter to definae style. 0 element is main color, all other elements are complemntary colors
+# cols = {
+#     "a" : [(0.00000, 0.04706, 0.03529), (0.64706, 0.41569, 0.21176), (0.84706, 0.81176, 0.78039), (0.84706, 0.65490, 0.58431), (0.54902,0.00784,0.00784)],
+#     "b" : [(0.62745,0.76471,0.84706), (0.13333,0.35686,0.44706), (0.19608,0.24706,0.00392), (0.84706,0.46667,0.38039), (0.74902,0.26667,0.26667)],
+#     "c" : [(0.31373,0.70588,0.74902), (0.84706,0.63922,0.01569), (0.74902,0.49020,0.01176), (0.74902,0.35686,0.01176), (0.64706,0.48235,0.33725)]
+# }
+letterstyles = 'abcdefghijkl'
+cols = {
+    "a" : [(0.0194444444444444,0.22,0.96),(0.569444444444444,0.16,0.96),(0.633333333333333,0.7,0.34),(0,0,0.68),(0,0,1),(0.611111111111111,0.38,0.03),(0.991666666666667,0.85,0.85),(0.111111111111111,0.13,0.99)],
+    "b" : [(0.997222222222222,0.68,0.82),(0.569444444444444,0.16,0.96),(0.633333333333333,0.7,0.34),(0,0,0.68),(0,0,1),(0.611111111111111,0.43,0.03),(0.0194444444444444,0.27,0.98),(0.111111111111111,0.13,0.98)],
+    "c" : [(0.0388888888888889,0.83,0.95),(0.402777777777778,0.81,0.35),(0.569444444444444,0.16,0.96),(0.633333333333333,0.7,0.34),(0,0,1),(0.611111111111111,0.43,0.03),(0.111111111111111,0.13,0.99),(0.0277777777777778,0.44,0.31)],
+    "d" : [(0.119444444444444,0.1,0.96),(0.633333333333333,0.7,0.34),(0.933333333333333,0.56,0.38),(0.0222222222222222,0.45,0.31),(0,0,1),(0.611111111111111,0.38,0.03),(0.136111111111111,0.81,0.99),(0.0388888888888889,0.83,0.95)],
+    "e" : [(0.130555555555556,0.69,0.98),(0.402777777777778,0.81,0.35),(0.633333333333333,0.7,0.34),(0,0,1),(0.611111111111111,0.38,0.03),(0.111111111111111,0.13,0.99)],
+    "f" : [(0.433333333333333,0.99,0.47),(0.0388888888888889,0.83,0.95),(0.933333333333333,0.56,0.38),(0,0,1),(0.611111111111111,0.38,0.03),(0.136111111111111,0.81,0.99),(0.569444444444444,0.16,0.96)],
+    "g" : [(0.563888888888889,0.15,0.93),(0.0194444444444444,0.27,0.98),(0.991666666666667,0.85,0.85),(0.0388888888888889,0.83,0.95),(0,0,1),(0.611111111111111,0.38,0.03),(0.633333333333333,0.7,0.34),(0.933333333333333,0.56,0.38)],
+    "h" : [(0.580555555555556,0.76,0.45),(0.0194444444444444,0.27,0.98),(0.991666666666667,0.85,0.85),(0.136111111111111,0.81,0.99),(0,0,0.68),(0,0,1),(0.611111111111111,0.38,0.03),(0.569444444444444,0.16,0.96),(0.933333333333333,0.56,0.38)],
+    "i" : [(0.925,0.39,0.45),(0.0388888888888889,0.83,0.95),(0,0,0.68),(0.433333333333333,0.99,0.47),(0,0,1),(0.611111111111111,0.38,0.03),(0.569444444444444,0.16,0.96),(0.633333333333333,0.7,0.34)],
+    "j" : [(0.025,0.28,0.39),(0.111111111111111,0.13,0.99),(0,0,1),(0.611111111111111,0.38,0.03),(0.0388888888888889,0.83,0.95)],
+    "k" : [(0.588888888888889,0.08,0.74),(0.0194444444444444,0.27,0.98),(0.991666666666667,0.85,0.85),(0.633333333333333,0.7,0.34),(0.933333333333333,0.56,0.38),(0,0,1),(0.611111111111111,0.38,0.03)],
+    "l" : [(0,0,1), (0,0,1), (0.611111111111111,0.38,0.03), (0.611111111111111,0.38,0.03)],
+}
+
+haircols = [(0.66667,0.53333,0.40000), (0.87059,0.74510,0.60000), (0.14118,0.10980,0.06667), (0.30980,0.10196,0.00000), (0.60392,0.20000,0.00000) ]
+skincols = [(0.310, 0.102, 0.000), (0.21403, 0.129142,0.019756), (0.227,0.062,0.0000), (0.841,0.431,0.195) ]
 
 def MonocromaticColor(col, numberCols):
 
@@ -20,7 +53,7 @@ def MonocromaticColor(col, numberCols):
         #so that the last color will have 1 value, and the first color will be black.
         #you could instead multiply this by i + 1,
         #so that the darkest color will be a dark shade of your hue rather than black.
-        v = (1/numColors) * (i + 0.5)
+        v = (1/numColors) * (i + 0.1)
         c = Color()
         c.hsv = (h, s, v)
         colors[i] = c.r, c.g, c.b
@@ -44,15 +77,15 @@ def AnalagousColor():
         h3 =  1 - (abs(h3) % 1)
     
     c0 = Color()
-    c0.hsv = (h1, 1, 1)
+    c0.hsv = (h1, random.uniform(0, 1), 1)
     colors[0] = (c0.r, c0.g, c0.b)
     
     c1 = Color()
-    c1.hsv = (h2, 1, 1)
+    c1.hsv = (h2, random.uniform(0.5, 1), 1)
     colors[1] = (c1.r, c1.g, c1.b)
     
     c2 = Color()
-    c2.hsv = (h3, 1, 1)
+    c2.hsv = (h3, random.uniform(0.9, 1), 1)
     colors[2] = (c2.r, c2.g, c2.b)
     
     return colors
@@ -102,3 +135,73 @@ def SplitComplmentaryColor():
     c2.hsv = (h3, 1, 1)
     colors[2] = (c2.r, c2.g, c2.b)
     return colors
+
+style = []
+styleChoice = ""
+
+maincolor = (0.0, 0.0, 0.0)
+secondarycolor = (0.0, 0.0, 0.0)
+
+mainColorIndex = -1
+SecondaryColorIndex = -1
+
+
+def SetUpCharacterStyle():
+    global style, styleChoice
+    global maincolor, secondarycolor
+    global mainColorIndex, SecondaryColorIndex
+
+    styleChoice = random.choice(letterstyles)    
+    style = copy.deepcopy( cols[random.choice(styleChoice)] )
+
+
+    if( random.random() > .05 ):
+        mainColorIndex = 0
+        #maincolor = style.pop(mainColorIndex)
+        maincolor = style[mainColorIndex]
+        SecondaryColorIndex = random.randrange(0, len(style))
+        #secondarycolor = style.pop(SecondaryColorIndex)
+        secondarycolor = style[SecondaryColorIndex]
+    else:
+        SecondaryColorIndex = 0
+        #secondarycolor = style.pop(SecondaryColorIndex)
+        secondarycolor = style[SecondaryColorIndex]
+        mainColorIndex = random.randrange(0, len(style))
+        #maincolor = style.pop(mainColorIndex)
+        maincolor = style[mainColorIndex]
+
+    
+    for child in bpy.data.collections.get("Kae").objects:
+        obj = bpy.data.objects[child.name]
+        obj["TestColor"] = skincols[random.randrange(len(skincols))]
+        obj["metallic"] = random.random()
+        obj.hide_viewport = False
+
+def PickOutfitColors(attribute, chidlrenObjs):
+    global style, styleChoice
+    global maincolor, secondarycolor
+    global mainColorIndex, SecondaryColorIndex
+
+    col = (0.0, 0.0, 0.0)
+    colIndex = -1
+    if(attribute == "01-UpperTorso"):
+        col =  maincolor
+        colIndex = mainColorIndex
+    elif(attribute == "08-PelvisThick" or attribute == "08-PelvisThick"):
+        col = secondarycolor
+        colIndex = SecondaryColorIndex
+    elif(attribute == "17-UpperHead"):
+        colIndex = random.randrange(0, len(haircols) ) 
+        col = haircols[colIndex]
+    else:
+        print(len(style))
+        colIndex = random.randrange(0, len(style) ) 
+        col = style[colIndex]
+        #col = (random.random(), random.random(), random.random())
+
+    for child in chidlrenObjs:
+        ob = bpy.data.objects[child.name]
+        c = Color()
+        c.hsv = col[0], col[1], col[2]
+        ob["TestColor"] = (c.r, c.g, c.b)
+    return colIndex
--- main/Outfit_Generator.py
+++ main/Outfit_Generator.py
@@ -11,6 +11,10 @@ import json
 import random
 import importlib
 from functools import partial
+from mathutils import Color
+
+from . import ColorGen
+importlib.reload(ColorGen)
 
 
 # A list for each caterogry of clothing that states what slots it will fil
@@ -32,14 +36,31 @@ ThickShortsSlot = ["08-PelvisThick", "09-PelvisThin"]
 ThinShortsSlot = ["09-PelvisThin"]
 NeckWearSlots = ["13-Neck"]
 
+#Color dict which uses a letter to definae style. 0 element is main color, all other elements are complemntary colors
+# cols = {
+#     "a" : [(0.00000, 0.04706, 0.03529), (0.64706, 0.41569, 0.21176), (0.84706, 0.81176, 0.78039), (0.84706, 0.65490, 0.58431), (0.54902,0.00784,0.00784)],
+#     "b" : [(0.62745,0.76471,0.84706), (0.13333,0.35686,0.44706), (0.19608,0.24706,0.00392), (0.84706,0.46667,0.38039), (0.74902,0.26667,0.26667)],
+#     "c" : [(0.31373,0.70588,0.74902), (0.84706,0.63922,0.01569), (0.74902,0.49020,0.01176), (0.74902,0.35686,0.01176), (0.64706,0.48235,0.33725)]
+# }
 cols = {
-    "a" : [(0.00000, 0.04706, 0.03529), (0.64706, 0.41569, 0.21176), (0.84706, 0.81176, 0.78039), (0.84706, 0.65490, 0.58431), (0.54902,0.00784,0.00784)],
-    "b" : [(0.62745,0.76471,0.84706), (0.13333,0.35686,0.44706), (0.19608,0.24706,0.00392), (0.84706,0.46667,0.38039), (0.74902,0.26667,0.26667)],
-    "c" : [(0.31373,0.70588,0.74902), (0.84706,0.63922,0.01569), (0.74902,0.49020,0.01176), (0.74902,0.35686,0.01176), (0.64706,0.48235,0.33725)]
+    "a" : [(0.0194444444444444,0.22,0.96),(0.569444444444444,0.16,0.96),(0.633333333333333,0.7,0.34),(0,0,0.68),(0,0,1),(0.611111111111111,0.38,0.03),(0.991666666666667,0.85,0.85),(0.111111111111111,0.13,0.99)],
+    "b" : [(0.997222222222222,0.68,0.82),(0.569444444444444,0.16,0.96),(0.633333333333333,0.7,0.34),(0,0,0.68),(0,0,1),(0.611111111111111,0.43,0.03),(0.0194444444444444,0.27,0.98),(0.111111111111111,0.13,0.98)],
+    "c" : [(0.0388888888888889,0.83,0.95),(0.402777777777778,0.81,0.35),(0.569444444444444,0.16,0.96),(0.633333333333333,0.7,0.34),(0,0,1),(0.611111111111111,0.43,0.03),(0.111111111111111,0.13,0.99),(0.0277777777777778,0.44,0.31)],
+    "d" : [(0.119444444444444,0.1,0.96),(0.633333333333333,0.7,0.34),(0.933333333333333,0.56,0.38),(0.0222222222222222,0.45,0.31),(0,0,1),(0.611111111111111,0.38,0.03),(0.136111111111111,0.81,0.99),(0.0388888888888889,0.83,0.95)],
+    "e" : [(0.130555555555556,0.69,0.98),(0.402777777777778,0.81,0.35),(0.633333333333333,0.7,0.34),(0,0,1),(0.611111111111111,0.38,0.03),(0.111111111111111,0.13,0.99)],
+    "f" : [(0.433333333333333,0.99,0.47),(0.0388888888888889,0.83,0.95),(0.933333333333333,0.56,0.38),(0,0,1),(0.611111111111111,0.38,0.03),(0.136111111111111,0.81,0.99),(0.569444444444444,0.16,0.96)],
+    "g" : [(0.563888888888889,0.15,0.93),(0.0194444444444444,0.27,0.98),(0.991666666666667,0.85,0.85),(0.0388888888888889,0.83,0.95),(0,0,1),(0.611111111111111,0.38,0.03),(0.633333333333333,0.7,0.34),(0.933333333333333,0.56,0.38)],
+    "h" : [(0.580555555555556,0.76,0.45),(0.0194444444444444,0.27,0.98),(0.991666666666667,0.85,0.85),(0.136111111111111,0.81,0.99),(0,0,0.68),(0,0,1),(0.611111111111111,0.38,0.03),(0.569444444444444,0.16,0.96),(0.933333333333333,0.56,0.38)],
+    "i" : [(0.925,0.39,0.45),(0.0388888888888889,0.83,0.95),(0,0,0.68),(0.433333333333333,0.99,0.47),(0,0,1),(0.611111111111111,0.38,0.03),(0.569444444444444,0.16,0.96),(0.633333333333333,0.7,0.34)],
+    "j" : [(0.025,0.28,0.39),(0.111111111111111,0.13,0.99),(0,0,1),(0.611111111111111,0.38,0.03),(0.0388888888888889,0.83,0.95)],
+    "k" : [(0.588888888888889,0.08,0.74),(0.0194444444444444,0.27,0.98),(0.991666666666667,0.85,0.85),(0.633333333333333,0.7,0.34),(0.933333333333333,0.56,0.38),(0,0,1),(0.611111111111111,0.38,0.03)],
+    "l" : [(0,0,1), (0,0,1), (0.611111111111111,0.38,0.03), (0.611111111111111,0.38,0.03)],
+
 }
 
 haircols = [(0.66667,0.53333,0.40000), (0.87059,0.74510,0.60000), (0.14118,0.10980,0.06667), (0.30980,0.10196,0.00000), (0.60392,0.20000,0.00000) ]
 skincols = [(0.310, 0.102, 0.000), (0.21403, 0.129142,0.019756), (0.227,0.062,0.0000), (0.841,0.431,0.195) ]
+
 # A dictionary which can be called to find what slots to fill when using certian items
 ItemUsedBodySlot = {
     "Coats": CoatSlots, 
@@ -77,15 +98,6 @@ def RandomizeSingleDNAStrandColor(inputSlot, CurrentDNA, save_path):
 
 def RandomizeSingleDNAStrandMesh(inputSlot, CurrentDNA, save_path):
 
-    #go through entire DNA and populate diction of used body slots
-    #Create a dictionary based on current top level collections in scene that should relate to slots. Set them to be populated false  
-
-    #go to index of slot chosen
-    #set body slots it populated to false
-    #get a random weight item from children
-    #check to see if its allowed with body slots 
-    #if so choose that item, if not pick again
-
     Blend_My_NFTs_Output = os.path.join(save_path, "Blend_My_NFTs Output", "NFT_Data")
     NFTRecord_save_path = os.path.join(Blend_My_NFTs_Output, "NFTRecord.json")
 
@@ -93,7 +105,6 @@ def RandomizeSingleDNAStrandMesh(inputSlot, CurrentDNA, save_path):
 
     hierarchy = DataDictionary["hierarchy"]
     DNAList = DataDictionary["DNAList"]
-    #print(CurrentDNA)
 
     
     #Build Dict
@@ -129,12 +140,10 @@ def RandomizeSingleDNAStrandMesh(inputSlot, CurrentDNA, save_path):
             attributes = slot
             indexToEdit =  strand
             currentVarient = variant
-    print(attributeUsedDict)
     attempts = 100
     while attempts > 0:
         typeChoosen, typeIndex = PickWeightedAttributeType(attributes[1])
         varientChoosen, varientIndex = PickWeightedTypeVarient(attributes[1][typeChoosen])
-        print(varientChoosen)
         if(varientChoosen != currentVarient):
             willItemFit =  True
             ItemClothingGenre = hierarchy[attributes[0]][typeChoosen][varientChoosen]["clothingGenre"]
@@ -144,10 +153,8 @@ def RandomizeSingleDNAStrandMesh(inputSlot, CurrentDNA, save_path):
                     if attributeUsedDict.get(i):
                         willItemFit = False
             if(willItemFit):       
-                print(varientChoosen)
                 DNAString[indexToEdit] = str(typeIndex) + "-" + str(varientIndex)
                 FormattedDNA = ','.join(DNAString)
-                print(FormattedDNA)
                 bpy.data.collections[currentVarient].hide_viewport = True
                 bpy.data.collections[varientChoosen].hide_viewport = False
                 return FormattedDNA
@@ -197,30 +204,32 @@ def RandomizeFullCharacter(maxNFTs, save_path):
         attributevalues = list(hierarchy.values())
         attributeUsedDict = dict.fromkeys(attributeskeys, False)
 
-        letterstyles = 'abc'
-        styleChoice = random.choice(letterstyles)
-        style = copy.deepcopy( cols[random.choice(styleChoice)] )
-        maincolor = (0.0, 0.0, 0.0)
-        secondarycolor = (0.0, 0.0, 0.0)
-        mainColorIndex = -1
-        SecondaryColorIndex = -1
-        if( random.random() > .05 ):
-            mainColorIndex = 0
-            maincolor = style.pop(mainColorIndex)
-            SecondaryColorIndex = random.randrange(0, len(style))
-            secondarycolor = style.pop(SecondaryColorIndex)
-        else:
-            SecondaryColorIndex = 0
-            secondarycolor = style.pop(SecondaryColorIndex)
-            mainColorIndex = random.randrange(0, len(style))
-            maincolor = style.pop(mainColorIndex)
+        ColorGen.SetUpCharacterStyle()
+
+        # letterstyles = 'abcdefghijkl'
+        # styleChoice = random.choice(letterstyles)
+        # style = copy.deepcopy( cols[random.choice(styleChoice)] )
+        # maincolor = (0.0, 0.0, 0.0)
+        # secondarycolor = (0.0, 0.0, 0.0)
+        # mainColorIndex = -1
+        # SecondaryColorIndex = -1
+        # if( random.random() > .05 ):
+        #     mainColorIndex = 0
+        #     maincolor = style.pop(mainColorIndex)
+        #     SecondaryColorIndex = random.randrange(0, len(style))
+        #     secondarycolor = style.pop(SecondaryColorIndex)
+        # else:
+        #     SecondaryColorIndex = 0
+        #     secondarycolor = style.pop(SecondaryColorIndex)
+        #     mainColorIndex = random.randrange(0, len(style))
+        #     maincolor = style.pop(mainColorIndex)
 
         
-        for child in bpy.data.collections.get("Kae").objects:
-            obj = bpy.data.objects[child.name]
-            obj["TestColor"] = skincols[random.randrange(len(skincols))]
-            obj["metallic"] = random.random()
-            obj.hide_viewport = False
+        # for child in bpy.data.collections.get("Kae").objects:
+        #     obj = bpy.data.objects[child.name]
+        #     obj["TestColor"] = skincols[random.randrange(len(skincols))]
+        #     obj["metallic"] = random.random()
+        #     obj.hide_viewport = False
         
          
 
@@ -243,26 +252,9 @@ def RandomizeFullCharacter(maxNFTs, save_path):
                 bpy.data.collections.get(varientChoosen).hide_viewport = False
                 chidlrenObjs = bpy.data.collections.get(varientChoosen).objects
 
-                col = (0.0, 0.0, 0.0)
-                colIndex = -1
-                if(attribute == "01-UpperTorso"):
-                    col =  maincolor
-                    colIndex = mainColorIndex
-                elif(attribute == "08-PelvisThick" or attribute == "08-PelvisThick"):
-                    col = secondarycolor
-                    colIndex = SecondaryColorIndex
-                elif(attribute == "17-UpperHead"):
-                    colIndex = random.randrange(0, len(haircols) ) 
-                    col = haircols[colIndex]
-                else:  
-                    col = style[random.randrange(0, len(style) ) ]
-                    #col = (random.random(), random.random(), random.random())
+                colIndex = ColorGen.PickOutfitColors(attribute, chidlrenObjs)
 
-                for child in chidlrenObjs:
-                    ob = bpy.data.objects[child.name]
-                    ob["TestColor"] = col
-
-                #SingleDNA[list(hierarchy.keys()).index(attribute)] = str(typeIndex) + "-" + str(varientIndex) + str(styleChoice) + str(colIndex)
+                #SingleDNA[list(hierarchy.keys()).index(attribute)] = str(typeIndex) + "-" + str(varientIndex) + str(ColorGen.styleChoice) + str(colIndex)
                 SingleDNA[list(hierarchy.keys()).index(attribute)] = str(typeIndex) + "-" + str(varientIndex)
                 VarientDict = {}
                 VarientDict[varientChoosen] = hierarchy[attribute][typeChoosen][varientChoosen]
